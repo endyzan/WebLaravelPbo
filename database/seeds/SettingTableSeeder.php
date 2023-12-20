bbefe32b9008c4487dac808533b51531b22a3acf
endyzan <andizainuri12@gmail.com>
2023-12-20 23:51:30 +0700

    logo

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -18,7 +18,8 @@ class SettingTableSeeder extends Seeder
             'short_des'=>"Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
             'photo'=>"image.jpg",
             'logo'=>'logo.jpg',
-            'address'=>"NO. 342 - London Oxford Street, 012 United Kingdom",
+            // 'logo'=>'logoagrijaya.jpg',
+            // 'logoagrijaya'=>'logoagrijaya.jpg','address'=>"NO. 342 - London Oxford Street, 012 United Kingdom",
             'email'=>"eshop@gmail.com",
             'phone'=>"+060 (800) 801-582",
         );
